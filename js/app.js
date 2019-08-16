b39c37ed72b73d118bfbe0f3b9f92bedf6e65c29
Rastko Dimitrijevic <raleliverpool@gmail.com>
2019-08-16 18:40:57 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Toggle class on click
+document.getElementById('cart-info').addEventListener('click', function() {
+  const cart = document.getElementById('cart');
+  cart.classList.toggle('show-cart');
+  console.log(cart);
+});
+
+// Back to top btn
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 650 ||
+    document.documentElement.scrollTop > 650
+  ) {
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
